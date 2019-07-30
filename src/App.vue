3b37d1fcc6e8ebcb9ee42b38805563e5fc03e16d
dufeng <2517838529@qq.com>
2019-07-30 08:46:52 +0800

    项目完成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">home</router-link> |
+      <router-link to="/about">About</router-link>
+       <div class="box">
+         
+
+       </div>
+
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,4 +22,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.box{
+  height: 300px;
+  width: 300px;
+  background-color: red;
+}
+
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>今天开始用vue做项目</h1>
+       <ul>
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+         <li>4</li>
+         <li>5</li>
+         <li>6</li>
+       </ul>
+  </div>
+</template>
